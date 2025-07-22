215ce76b660e29b4a377368f1eb5b724ab751a7b
lucky934-raj <luckyrajsinghchundawat7@gmail.com>
2025-07-22 14:09:11 +0530

    Delete backend directory

--- backend/routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router= express.Router();
-const { generateFeedback } = require("../controllers/feedbackController.js");
-const verifyToken = require("../middlewares/authMiddleware");
-router.post("/generate",verifyToken, generateFeedback);
-// This route allows users to submit feedback on a module
-// It requires the user to be authenticated (verifyToken middleware)
-module.exports = router;
-// This exports the router so it can be used in the main server file
--- backend/routes/judgeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const verifyToken = require("../middlewares/authMiddleware");
-const { judgeSpeech } = require("../controllers/judgeController");
-router.post("/score", verifyToken, judgeSpeech);
-
-module.exports = router;
