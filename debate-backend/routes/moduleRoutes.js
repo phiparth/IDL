ada05b6d3de20c1be5aab7608cb967212931eefd
lucky934-raj <luckyrajsinghchundawat7@gmail.com>
2025-07-23 07:14:05 +0530

    Delete debate-backend directory

--- debate-backend/routes/moduleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getModules } = require("../controllers/modulecontroller");
-const verifyToken = require("../middlewares/authMiddleware");
-// GET /api/modules
-router.get("/", verifyToken,getModules);
-
-module.exports = router;
--- debate-backend/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middlewares/authMiddleware");
-const { submitQuiz } = require("../controllers/quizController");
-
-router.post("/submit", verifyToken, submitQuiz);
-
-module.exports = router;
-
